fae3f5de18e6249da9f4f7fc45bcfc1a7a97c28b
petyusa <peter.vincze.dev@gmail.com>
2018-12-14 17:27:45 +0100

    refactored pagination tag helper

--- Vinance.Web/TagHelpers/PaginationAjaxTagHelper.cs
+++ Vinance.Web/TagHelpers/PaginationAjaxTagHelper.cs
@@ -18,9 +18,9 @@ namespace Vinance.Web.TagHelpers
 
         public override void Process(TagHelperContext context, TagHelperOutput output)
         {
-            output.TagName = "div";
-            AddContent(output);
+            output.TagName = "nav";
             output.TagMode = TagMode.StartTagAndEndTag;
+            AddContent(output);
         }
 
         private void AddContent(TagHelperOutput output)
@@ -50,25 +50,80 @@ namespace Vinance.Web.TagHelpers
                 sb.Append($"pagesize={Pagesize}");
                 sb.Append("&");
             }
+            
+            var firstPage = $@"<a class='page-link'
+                    href='{sb}page=1' 
+                    data-ajax-method='GET'
+                    data-ajax='true'
+                    data-ajax-update='#{TargetId}'
+                    data-ajax-mode='replace'><i data-feather='chevrons-left'></i></a>";
+            var previousPage = $@"<a class='page-link'
+                    href='{sb}page={Page - 1}' 
+                    data-ajax-method='GET'
+                    data-ajax='true'
+                    data-ajax-update='#{TargetId}'
+                    data-ajax-mode='replace'><i data-feather='chevron-left'></i></a>";
+            var nextPage = $@"<a class='page-link'
+                    href='{sb}page={Page + 1}' 
+                    data-ajax-method='GET'
+                    data-ajax='true'
+                    data-ajax-update='#{TargetId}'
+                    data-ajax-mode='replace'><i data-feather='chevron-right'></i></a>";
+                var lastPage = $@"<a class='page-link'
+                    href='{sb}page={TotalPages}' 
+                    data-ajax-method='GET'
+                    data-ajax='true'
+                    data-ajax-update='#{TargetId}'
+                    data-ajax-mode='replace'><i data-feather='chevrons-right'></i></a>";
 
-            var html = $@"
-            <a href='{sb}page=1' data-ajax-method='GET'
-                data-ajax='true'
-                data-ajax-update='#{TargetId}'
-                data-ajax-mode='replace'><i data-feather='chevrons-left'></i></a>
-            <a href='{sb}page={(Page == 1 ? 1 : Page - 1)}' data-ajax-method='GET'
-                data-ajax='true'
-                data-ajax-update='#{TargetId}'
-                data-ajax-mode='replace'><i data-feather='chevron-left'></i></a>
-            <a href='{sb}page={(Page == TotalPages ? TotalPages : Page + 1)}' data-ajax-method='GET'
-                data-ajax='true'
-                data-ajax-update='#{TargetId}'
-                data-ajax-mode='replace'><i data-feather='chevron-right'></i></a>
-            <a href='{sb}page={TotalPages}' data-ajax-method='GET'
-                data-ajax='true'
-                data-ajax-update='#{TargetId}'
-                data-ajax-mode='replace'><i data-feather='chevrons-right'></i></a>";
-            output.Content.SetHtmlContent(html);
+            const string ulBegin = "<ul class='pagination justify-content-end'>";
+
+            const string li = "<li class='page-item'>";
+            const string liActive = "<li class='page-item active'>";
+            const string liDisabled = "<li class='page-item disabled'>";
+
+            const string liEnd = "</li>";
+
+            const string ulEnd = "</ul>";
+
+            var htmlBuilder = new StringBuilder();
+            htmlBuilder.Append(ulBegin);
+
+            htmlBuilder.Append(Page == 1 ? liDisabled : li);
+            htmlBuilder.Append(firstPage);
+            htmlBuilder.Append(liEnd);
+
+            htmlBuilder.Append(Page == 1 ? liDisabled : li);
+            htmlBuilder.Append(previousPage);
+            htmlBuilder.Append(liEnd);
+
+            for (var i = Page - 2; i <= Page + 2; i++)
+            {
+                if (i > 0 && i <= TotalPages)
+                {
+                    htmlBuilder.Append(i == Page ? liActive : li);
+                    htmlBuilder.Append($@"<a class='page-link'
+                        href='{sb}page={i}' 
+                        data-ajax-method='GET'
+                        data-ajax='true'
+                        data-ajax-update='#{TargetId}'
+                        data-ajax-mode='replace'>{i}</a>");
+                    htmlBuilder.Append(liEnd);
+                }
+
+            }
+
+            htmlBuilder.Append(Page == TotalPages ? liDisabled : li);
+            htmlBuilder.Append(nextPage);
+            htmlBuilder.Append(liEnd);
+
+            htmlBuilder.Append(Page == TotalPages ? liDisabled : li);
+            htmlBuilder.Append(lastPage);
+            htmlBuilder.Append(liEnd);
+
+            htmlBuilder.Append(ulEnd);
+
+            output.Content.SetHtmlContent(htmlBuilder.ToString());
         }
     }
 }
